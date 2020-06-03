c7598330c54bd1c1ead7be9f1b4b424bae1e2812
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-06-03 18:25:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ document.querySelector('main').remove()
 const newHeader = document.createElement('h1')
 
 newHeader.id = 'victory'
+<<<<<<< HEAD
 
 newHeader.innerHTML = 'YOUR-NAME is the champion'
+=======
+>>>>>>> 41c62332129f28b40a982aa73c327f21b33d08a3
